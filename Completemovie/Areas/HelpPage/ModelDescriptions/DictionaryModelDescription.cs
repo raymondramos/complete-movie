82e694b2a39e6f0ec2c512095999956a1bd7c8b9
Raymond Aaron Ramos <raymondaaronramos@gmail.com>
2023-10-14 09:06:18 +0800

    first commit

---
+++ Completemovie/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Completemovie.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Completemovie/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Completemovie.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
